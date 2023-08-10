706cf13696ff19f4170c5fd1f3cd233e33a736a7
Walter Arias <walter.arias.aguirre@gmail.com>
2023-08-10 07:10:08 -0500

    objetos

--- 12. objetos/objetos.js
+++ 12. objetos/objetos.js
@@ -6,21 +6,41 @@ let aprendices = [
     nombre: "Anderson Tasama",
     edad: 20,
     estaActivo: true,
+    colores: ["red", "azul", "rosado", true],
+    materias: {
+      ingles: 2.3,
+      programacion: 5,
+      etica: 4.8,
+    },
   },
   {
-    id: 139,
-    nombre: "Juan PSP VPN ",
-    edad: 10,
-    estaActivo: false,
-  },
-  {
-    id: 139,
-    nombre: "Kevin Alzate ",
-    edad: 22,
-    estaActivo: true,
+    id: 200,
+    nombre: "Felipe Perez",
   },
 ];
 
-aprendices.forEach((aprendiz) => {
+// operador de objetos de javascript : .
+/* aprendices.nombre = "Pedro Perez";
+aprendices.colores[0] = "rojo";
+aprendices.materias.ingles = 2.8;
+console.log(aprendices.materias);
+ */
+/*  aprendices.forEach((aprendiz) => {
   document.write(`${aprendiz.id} - ${aprendiz.nombre} <br>`);
+}); */
+
+aprendices.push({ id: 400, nombre: "Jose Gomez" });
+//console.log(aprendices);
+
+// formas clasica
+/* aprendices.forEach((aprendiz) => {
+  document.write(`${aprendiz.id} - ${aprendiz.nombre} <br>`);
+});
+ */
+//forma 2
+
+Object.entries(aprendices).forEach(([llave, valor]) => {
+  if (valor.nombre === "Felipe Perez") {
+    console.log(valor.nombre);
+  }
 });
