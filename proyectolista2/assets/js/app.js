5e133456e1587b585b1fe885d467ae571cadb421
walter.arias <walter.arias.aguirre@gmail.com>
2023-10-09 08:28:12 -0500

    refresco de pantalla

--- proyectolista2/assets/js/app.js
+++ proyectolista2/assets/js/app.js
@@ -5,7 +5,6 @@ let miTabla = document.getElementById("miTabla");
 
 // instanciamos el almacenamiento local
 const db = window.localStorage;
-
 // CRUD en localstorage  Create Update Delete
 
 const crear = () => {
@@ -18,4 +17,33 @@ const crear = () => {
     icon: "success",
     confirmButtonText: "Oki",
   });
+  window.location.href = "http://127.0.0.1:5500/proyectolista2/";
 };
+
+const borrarDb = () => {
+  Swal.fire({
+    title: "Esta seguro?",
+    text: "Atención: esta operacion es irreversible!",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Borrar !",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      db.clear();
+      Swal.fire("Borrado !", "Base de datos eliminada", "success");
+      window.location.href = "http://127.0.0.1:5500/proyectolista2/";
+    }
+  });
+};
+
+const listarDb = () => {
+  let llaves = Object.keys(db);
+  llaves.map((llave) => {
+    let tareas = JSON.parse(db.getItem(llave));
+    let fila = `<tr><td>${llave}</td><td>${tareas.tarea}</td><td>${tareas.detalle}</td></tr>`;
+    miTabla.innerHTML += fila;
+  });
+};
+listarDb();
